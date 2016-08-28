0d42e8d2f6dc9c32ceefbe2fa6f9cdf46e3a80b9
Kaichao Sun <kcsun@thoughtworks.com>
2016-08-28 21:11:34 +0800

    groups#update and #delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,11 +11,29 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def create
     @group = Group.new(group_params)
     @group.save
 
     redirect_to groups_path
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "更新成功"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "组织已被删除!"
   end
 
   private
